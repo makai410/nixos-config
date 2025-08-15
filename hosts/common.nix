85dca04510cbe07da709bd256cb04176a5a62a98
Makai <m4kai410@gmail.com>
2025-08-15 17:19:20 +0800

    idu

--- hosts/common.nix
+++ hosts/common.nix
@@ -55,16 +55,12 @@
     (symlinkJoin {
       name = "QQ";
       paths = [ qq ];
-      inherit (qq) meta;
+      inherit (qq) pname version meta;
       buildInputs = [ makeWrapper ];
       postBuild = ''
-        rm $out/share/applications/qq.desktop
-        cp ${qq}/share/applications/qq.desktop $out/share/applications/qq.desktop
-        substituteInPlace $out/share/applications/qq.desktop \
-          --replace-fail "${qq}" $out
-        wrapProgram $out/bin/qq \
-          --set NIXOS_OZONE_WL 1 \
-          --add-flags '--wayland-text-input-version=3'
+        wrapProgram "$out/bin/qq" \
+          --add-flags --enable-wayland-ime \
+          --add-flags --wayland-text-input-version=3
       '';
     })
     (symlinkJoin {
