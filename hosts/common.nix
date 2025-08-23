beb47a9640bf055c153f4e43f25471be3d777384
Makai <m4kai410@gmail.com>
2025-08-24 01:22:05 +0800

    a

--- hosts/common.nix
+++ hosts/common.nix
@@ -64,25 +64,5 @@
       '';
     })
     rnote
-    (
-      let
-        base = pkgs.appimageTools.defaultFhsEnvArgs;
-      in
-      pkgs.buildFHSEnv (
-        base
-        // {
-          name = "fhs";
-          targetPkgs =
-            pkgs:
-            (base.targetPkgs pkgs)
-            ++ (with pkgs; [
-              pkg-config
-            ]);
-          profile = "export FHS=1";
-          runScript = "fish";
-          extraOutputsToInstall = [ "dev" ];
-        }
-      )
-    )
   ];
 }
